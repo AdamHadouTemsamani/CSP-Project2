ce33325583514682250e6875ca249d9372916939
Philip Mørch <mrphil2105@gmail.com>
2025-05-19 19:10:29 +0200

    Add check for ensuring list is sorted

--- Rust-Project/merge_sort_perf/src/main.rs
+++ Rust-Project/merge_sort_perf/src/main.rs
@@ -12,16 +12,26 @@ use merge_sort::parallel_merge_sort;
 
 fn main() {
     let (max_threads, array_size) = parse_input();
-    let mut numbers = read_integers();
+    let mut integers = read_integers();
     let pool = ThreadPoolBuilder::new()
         .num_threads(max_threads)
         .build()
         .unwrap();
     let start = Instant::now();
     pool.install(|| {
-        parallel_merge_sort(&mut numbers);
+        parallel_merge_sort(&mut integers);
     });
     let duration = start.elapsed();
+    let last = 0;
+    for i in 0..array_size {
+        if (integers[i] < last) && (i > 0) {
+            eprintln!(
+                "Array is not sorted at index {}: {} < {}",
+                i, integers[i], last
+            );
+            process::exit(1);
+        }
+    }
     let secs = duration.as_secs_f64();
     let mips = (array_size as f64) / secs / 1_000_000.0;
     println!("{},{},{:.6},{:.3}", max_threads, array_size, secs, mips);
