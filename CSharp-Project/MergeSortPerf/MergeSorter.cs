99905b79a8814ba991ae34f51c5387ed603604da
Philip Mørch <mrphil2105@gmail.com>
2025-05-19 15:26:40 +0200

    Read integers from binary file for C#

--- CSharp-Project/MergeSortPerf/MergeSorter.cs
+++ CSharp-Project/MergeSortPerf/MergeSorter.cs
@@ -1,52 +1,53 @@
-using System;
-using System.Threading.Tasks;
-using System.Threading;
+namespace MergeSortPerf;
 
-namespace ParallelMergeSort
+public static class MergeSorter
 {
-    public static class MergeSorter
-    {
-        private static int counter = 0;
-        public static Task SortAsync(int[] arr) => MergeSortAsync(arr, 0, arr.Length - 1);
+    public static Task SortAsync(uint[] arr) => MergeSortAsync(arr, 0, arr.Length - 1);
 
-        public static async Task MergeSortAsync(int[] arr, int left, int right)
+    public static async Task MergeSortAsync(uint[] arr, int left, int right)
+    {
+        if (left >= right)
         {
-            if (left >= right) return;
-            
-            int mid = left + (right - left) / 2;
-            
-            int num = Interlocked.Increment(ref counter);
-            Console.WriteLine("Before Yield {0} - ThreadId {1}", num, Thread.CurrentThread.ManagedThreadId);
-            //Yield -- per parallel split
-            await Task.Yield();
-            Console.WriteLine("After Yield {0} - ThreadId {1}", num, Thread.CurrentThread.ManagedThreadId);
-            
-            var leftTask = MergeSortAsync(arr, left, mid);
-            var rightTask = MergeSortAsync(arr, mid + 1, right);
-            
-            await Task.WhenAll(leftTask, rightTask);
-            Merge(arr, left, mid, right);
+            return;
         }
 
-        private static void Merge(int[] arr, int left, int mid, int right)
-        {   
-            //Compute Length
-            int leftLength = mid - left + 1;
-            int rightLength = right - mid;
-            
-            //Allocate temp arrays and copy data
-            var lTemp = new int[leftLength];
-            var rTemp = new int[rightLength];
-            Array.Copy(arr, left, lTemp, 0, leftLength);
-            Array.Copy(arr, mid + 1, rTemp, 0, rightLength);
-            
-            //Merge
-            int i = 0, j = 0, k = left;
-            while (i < leftLength && j < rightLength)
-                arr[k++] = (lTemp[i] <= rTemp[j]) ? lTemp[i++] : rTemp[j++];
-            while (i < leftLength) arr[k++] = lTemp[i++];
-            while (j < rightLength) arr[k++] = rTemp[j++];
+        var mid = left + (right - left) / 2;
 
+        // Yield to enable concurrent computation
+        await Task.Yield();
+
+        var leftTask = MergeSortAsync(arr, left, mid);
+        var rightTask = MergeSortAsync(arr, mid + 1, right);
+
+        await Task.WhenAll(leftTask, rightTask);
+        Merge(arr, left, mid, right);
+    }
+
+    private static void Merge(uint[] arr, int left, int mid, int right)
+    {
+        var leftLength = mid - left + 1;
+        var rightLength = right - mid;
+
+        var leftTemp = new uint[leftLength];
+        var rightTemp = new uint[rightLength];
+        Array.Copy(arr, left, leftTemp, 0, leftLength);
+        Array.Copy(arr, mid + 1, rightTemp, 0, rightLength);
+
+        var i = 0;
+        var j = 0;
+        var k = left;
+
+        while (i < leftLength && j < rightLength)
+        {
+            arr[k++] = (leftTemp[i] <= rightTemp[j]) ? leftTemp[i++] : rightTemp[j++];
+        }
+        while (i < leftLength)
+        {
+            arr[k++] = leftTemp[i++];
+        }
+        while (j < rightLength)
+        {
+            arr[k++] = rightTemp[j++];
         }
     }
 }
