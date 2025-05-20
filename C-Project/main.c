6b6782d098d3eae6f01fbb5c0dccd89e6aa90745
ChrBank <chbl@itu.dk>
2025-05-20 14:04:30 +0200

    fix argument error

--- C-Project/main.c
+++ C-Project/main.c
@@ -19,21 +19,21 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    uint32_t *a = malloc(n * sizeof *a);
+    uint32_t *a = malloc(array_size * sizeof *a);
     if (!a) {
         perror("malloc array");
         return EXIT_FAILURE;
     }
 
     FILE *f = fopen("../random_integers.bin", "rb");
-    if (!f || fread(a, sizeof *a, n, f) != n) {
+    if (!f || fread(a, sizeof *a, array_size, f) != array_size) {
         perror("reading input");
         return EXIT_FAILURE;
     }
     fclose(f);
 
     omp_set_num_threads(max_threads);
-    parallel_merge_sort_init(n);
+    parallel_merge_sort_init(array_size);
 
     struct timespec t0, t1;
     clock_gettime(CLOCK_MONOTONIC, &t0);
@@ -41,15 +41,15 @@ int main(int argc, char **argv) {
     #pragma omp parallel
     {
         #pragma omp single nowait
-        parallel_merge_sort_omp(a, 0, n);
+        parallel_merge_sort_omp(a, 0, array_size);
     }
 
     clock_gettime(CLOCK_MONOTONIC, &t1);
     double secs = (t1.tv_sec - t0.tv_sec)
                 + (t1.tv_nsec - t0.tv_nsec) * 1e-9;
-    double mips = (n / secs) / 1e6;
+    double mips = (array_size / secs) / 1e6;
     printf("%d,%zu,%.6f,%.3f\n",
-           max_threads, n, secs, mips);
+           max_threads, array_size, secs, mips);
 
     parallel_merge_sort_fini();
     free(a);
